8b8192c6b44ada9d918bc0664030a0aa85c62e0a
Kate Haffenden <kate.haffenden@smartpension.co.uk>
2021-09-15 10:17:41 +0100

    Extract class into tip_calculator

---
+++ exercise-3/tips_processor.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class TipsProcessor
+
+  def initialize(form:)
+    @form = form
+  end
+
+  def call
+    SmsNotifier.new.call(text: sms_text)
+  end
+
+  private
+
+  attr_reader :form
+
+  def sms_text
+    "You tips have been processed. Total amount is: #{total_amount}"
+  end
+
+  def tipper
+    @tipper ||= TipCalculator.new(form.amount, form.discount_percentage, form.tip_percentage)
+  end
+
+  def total_amount
+    @tipper.total
+  end
+
+end
